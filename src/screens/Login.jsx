b82c41fa1cb8f316b1f5358329eb486d1a4904c2
Gerard C. Saperas <60043225+gerardcsaperas@users.noreply.github.com>
2022-09-15 09:00:03 +0200

    init auth

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import useAuth from '../hooks/useAuth';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import secrets from '../../secrets';
 
+const GOOGLE_WEB_ID = process.env.GOOGLE_WEB_ID || secrets.GOOGLE_WEB_ID;
+const GOOGLE_IOS_ID = process.env.GOOGLE_IOS_ID || secrets.GOOGLE_IOS_ID;
+const GOOGLE_ANDROID_ID =
+    process.env.GOOGLE_ANDROID_ID || secrets.GOOGLE_ANDROID_ID;
+
+WebBrowser.maybeCompleteAuthSession();
 const Login = () => {
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        expoClientId: GOOGLE_WEB_ID,
+        webClientId: GOOGLE_WEB_ID,
+        iosClientId: GOOGLE_IOS_ID,
+        androidClientId: GOOGLE_ANDROID_ID
+    });
+
+    useEffect(() => {
+        if (response?.type === 'success') {
+            const { authentication } = response;
+            console.log(authentication);
+        }
+    }, [response]);
+
     return (
         <View>
-            <Text>Login</Text>
+            <Button
+                title="Sign in with Google"
+                // leave `showInRecents: true` option for Android
+                onPress={() => promptAsync({ showInRecents: true })}
+            />
         </View>
     );
 };
