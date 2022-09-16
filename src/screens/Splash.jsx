f700a8bb887eb5897dbb0ca4fdd30bf799910103
Gerard C. Saperas <60043225+gerardcsaperas@users.noreply.github.com>
2022-09-16 09:28:48 +0200

    init Splash

---
+++ src/screens/Splash.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Splash = () => {
+    return (
+        <View style={{ backgroundColor: 'purple', height: '100%' }}>
+            <Text>Splash</Text>
+        </View>
+    );
+};
+
+export default Splash;
